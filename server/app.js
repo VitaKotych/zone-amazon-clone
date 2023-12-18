6b6a1af7d932ae0707c52af7804fd3c7f11f226d
VitaKotych <vitakotic4@gmail.com>
2023-12-18 15:50:44 +0200

    all

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const port = process.env.PORT || 8005 ;
+const port = process.env.PORT || 8000 ;
 const cookieParser = require("cookie-parser");
 const DefaultData = require("./defaultdata");
 require("./db/conn");
